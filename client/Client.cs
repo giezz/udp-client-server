ad5899e9142bf4ee64641afd818c807a5d45fc9a
giezz <giezz@vk.com>
2023-05-28 13:02:43 +0300

    Initial commit

---
+++ client/Client.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace client
+{
+    internal class Client
+    {
+        private const int port = 8080;
+        public static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient();
+            udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, port));
+            Console.WriteLine("UDP-клиент запущен. Ожидание сообщений от сервера...");
+
+            try
+            {
+                while (true)
+                {
+                    IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);;
+                    
+                    // receive file name
+                    byte[] data = udpClient.Receive(ref serverEP);
+                    string fileName = Encoding.UTF8.GetString(data);
+                    
+                    // receive file size
+                    byte[] fileSizeBytes = udpClient.Receive(ref serverEP);
+                    long fileSize = BitConverter.ToInt64(fileSizeBytes, 0);
+                    
+                    // receive file itself
+                    using (FileStream fileStream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+                    {
+                        int bytesRead;
+                        long bytesReceived = 0;
+
+                        while (bytesReceived < fileSize)
+                        {
+                            IPEndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                            byte[] receivedData = udpClient.Receive(ref senderEndPoint);
+                            bytesRead = receivedData.Length;
+                            
+                            fileStream.Write(receivedData, 0, bytesRead);
+
+                            bytesReceived += bytesRead;
+                        }
+                        
+                        Console.WriteLine("Файл успешно принят и сохранен.");
+                        Process.Start(fileStream.Name);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+            }
+            finally
+            {
+                udpClient.Close();
+            }
+        }
+    }
+}
---
+++ server/Server.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace server;
+
+class Server
+{
+    private static string[] files = new[] {"file_doc.txt", "file_img.jpg", "file_web.html"};
+    private const int port = 8080;
+
+    public static void Main(string[] args)
+    {
+        UdpClient udpServer = new UdpClient();
+        udpServer.EnableBroadcast = true;
+
+        try
+        {
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "/sr")
+                {
+                    SendRandomFile(udpServer);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Ошибка: " + e.Message);
+        }
+        finally
+        {
+            udpServer.Close();
+        }
+    }
+
+    public static void SendRandomFile(UdpClient server)
+    {
+        Random random = new Random();
+        string fileName = files[random.Next(3)];
+        
+        // send file name
+        byte[] data = Encoding.UTF8.GetBytes(fileName);
+        server.Send(data, data.Length, new IPEndPoint(IPAddress.Broadcast, port));
+
+        // send file size
+        FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+        long fileSize = fileStream.Length;
+        byte[] fileSizeBytes = BitConverter.GetBytes(fileSize);
+        server.Send(fileSizeBytes, fileSizeBytes.Length, new IPEndPoint(IPAddress.Broadcast, port));
+
+        // send file itself
+        int bufferSize = 1024;
+        byte[] buffer = new byte[bufferSize];
+        int bytesRead;
+        while ((bytesRead = fileStream.Read(buffer, 0, bufferSize)) > 0)
+        {
+            server.Send(buffer, bytesRead, new IPEndPoint(IPAddress.Broadcast, port));
+        }
+        
+        fileStream.Close();
+        Console.WriteLine("Файл отправлено клиентам.");
+    }
+}
